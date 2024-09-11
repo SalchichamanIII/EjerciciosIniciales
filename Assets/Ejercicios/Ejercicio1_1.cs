be8f4c67c4d2b23bd07ad7a5d112888125cd2b05
SalchichamanIII <franhp10b@gmail.com>
2024-09-11 18:20:18 +0200

    110924

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    //Ctrl + k  Ctrl + C Comentar
+    //Ctrl + k  Ctrl + U Descomentar
+    int vidas;
+    string nombrePersonaje;
+    bool veneno;
+    float porcentajeAcierto;
+    char solucionPuzzle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola mundo");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Adios mundo :(");
     }
 }
