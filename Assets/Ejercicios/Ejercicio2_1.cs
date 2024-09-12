efb5a72a7d6381a0f88fc029738f2100522ebce0
SalchichamanIII <franhp10b@gmail.com>
2024-09-12 19:18:55 +0200

    120924

--- Assets/Ejercicios/Ejercicio2_1.cs
+++ Assets/Ejercicios/Ejercicio2_1.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Ejercicio2_1 : MonoBehaviour
 {
-    string nombre = "Francisco" , matriculaCoche ="6880 mmq"  ;
-    int edad = 18 , peso =68;
-    float altura = 1.70f ;
-    bool alumnoRepetidor = false ;
-    char letra = 'f'; 
+    string nombre = "Francisco" , matriculaCoche ="6880 mmq" , mensaje = "Que tal llevas el examen?" ;
+    int edad = 18 , peso =68 , códigoPostal = 28911 , numeroDeHijos = 0 , talla = 38 , mayorEdad = 19 , dias = 31;
+    float altura = 1.70f , minutos = 12.20f , precio = 44.80f , contador = 66.12f ;
+    bool alumnoRepetidor = false , mayorDeEdad = true ;
+    char letra = 'f' , genero = 'H' , tallaCamiseta = 'M'; 
         
 
 
