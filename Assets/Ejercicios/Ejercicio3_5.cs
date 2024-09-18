47ef100a4c5d2621c9a627bec1d354551d8e9e2d
SalchichamanIII <franhp10b@gmail.com>
2024-09-18 20:26:28 +0200

    s

--- Assets/Ejercicios/Ejercicio3_5.cs
+++ Assets/Ejercicios/Ejercicio3_5.cs
@@ -8,7 +8,10 @@ public class Ejercicio3_5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float longitud = 2 * 3.14f * radio;
+        Debug.Log(longitud);
+        float area = 3.14f * radio * radio;
+        Debug.Log(area);
     }
 
     // Update is called once per frame
