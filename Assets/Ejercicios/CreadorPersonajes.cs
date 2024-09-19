e37fa715cc6aa03cb881080cab515cbcaf877610
SalchichamanIII <franhp10b@gmail.com>
2024-09-19 19:17:53 +0200

    19924

---
+++ Assets/Ejercicios/CreadorPersonajes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreadorPersonajes : MonoBehaviour
+{
+    int numero = 5;
+    string texto = "chalao";
+    float num = 3.4f;
+    Personaje Ronaldo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicios/Personaje.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    //Como se define mi personaje
+    //Propiedades o atributos
+    string nombre;
+    float vida;
+    float danho;
+    float velocidad;
+
+    public float Vida { get => vida; set => vida = value; }
+
+    //Que puede hacer mi personaje
+    void atacar()
+    {
+
+
+    }
+
+    void mover()
+    {
+
+    }
+
+    void Saltar()
+    {
+
+
+    }
+
+
+
+
+
+}
